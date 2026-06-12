4abf7e319347492e407ee661bfbcb661dbe7766d
JSMblWb <dmrybal4enko@gmail.com>
2026-06-13 01:34:22 +0700

    подготовил чай

---
+++ ciphers/tea/tea.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cstring>
+#include <random>
+#include <stdexcept>
+
+#define BUFFER_SIZE 4096
+
+#ifdef _WIN32
+	#ifdef BUILD
+		#define EXPORT __declspec(dllexport)
+	#else
+		#define EXPORT __declspec(dllimport)
+	#endif
+#else
+	#define EXPORT __attribute__((visibility("default")))
+#endif
+
+class EXPORT TEA {
+	public:
+	int cipherFunc(int mode, std::string keyHex, std::string inFile, std::string outFile);
+}
