ac4b198e0cc32850cbc0eba7cec5caf40bface5b
tascarit <134831942+tascarit@users.noreply.github.com>
2026-06-12 17:27:50 +0700

    Поправлен файл linuxBuilderArm.sh

--- linuxBuilderArm.sh
+++ linuxBuilderArm.sh
@@ -2,6 +2,6 @@
 
 
 g++ -fPIC -shared -o libs/libVernamArm.so ciphers/vernam/vernam.cpp
-g++ -fPIC -shared -o libs/libVigenereArm.so
+g++ -fPIC -shared -o libs/libVigenereArm.so ciphers/vigenere/vigenere.cpp
 
 g++ launcher.cpp -o cryptDeckArm libs/libVernamArm.so
