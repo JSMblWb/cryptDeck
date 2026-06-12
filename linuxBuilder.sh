857e01a6e501e2e48bf807fe467f49c01a3778a7
tascarit <saiqe@internet.ru>
2026-06-12 14:05:37 -0400

    Добавлена обработка шифрования в launcher.cpp (двойной перестановки)

--- linuxBuilder.sh
+++ linuxBuilder.sh
@@ -6,4 +6,4 @@ g++ -fPIC -shared -o libs/build/libVigenere.so ciphers/vigenere/vigenere.cpp
 g++ -fPIC -shared -o libs/build/libKeygen.so keygen.cpp
 g++ -fPIC -shared -o libs/build/libDoubleTransposition.so ciphers/double-transposition/double-transposition.cpp
 
-g++ launcher.cpp -o cryptDeck libs/build/libKeygen.so libs/build/libVernam.so libs/build/libVigenere.so
+g++ launcher.cpp -o cryptDeck libs/build/libKeygen.so libs/build/libVernam.so libs/build/libVigenere.so libs/build/libDoubleTransposition.so
